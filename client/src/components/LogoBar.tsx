e37eef3e319ae9b1bc76d49af49c2bd058acb426
Logan Johnston <ltjohnston92@gmail.com>
2020-03-23 21:38:20 -0700

    wip

--- client/src/components/LogoBar.tsx
+++ client/src/components/LogoBar.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import {Button } from "semantic-ui-react"
 import {Link } from "react-router-dom"
+import MogLogo from "../images/mog.svg"
 import "../styling/LogoBar.scss"
 
 
 export const LogoBar = () => {
    return  <div className="logobar">
       <div className="logobar-container">
-         
-         
+         <div className="logobar-left">
+      <img className="logobar-logo" src={MogLogo} />
+      </div>
          <div className="logobar-right">
          <Link to="/login"><Button className="logobar-login" >Login</Button></Link>
          <Link to="/sign-up"><Button className="logobar-signup"  primary>Sign Up</Button></Link>
